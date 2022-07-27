30a00d3f1cca1ceaa5d5423ba423d5c9f727f3c7
TGoddessana <ingstor1092@gmail.com>
2022-07-27 22:44:10 +0900

    :package: plinic, accounts app 생성, rest-framework 설치

--- backend/backend/settings/common.py
+++ backend/backend/settings/common.py
@@ -31,12 +31,18 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    # django apps
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # third apps
+    'rest_framework',
+    # local apps
+    'plinic',
+    'accounts'
 ]
 
 MIDDLEWARE = [
