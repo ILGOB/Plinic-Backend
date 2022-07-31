63087ee42713b157a20562b8f9b231dacbeea9ac
TGoddessana <ingstor1092@gmail.com>
2022-08-01 03:24:07 +0900

    :sparkles: 포스트 목록 api

--- backend/plinic/serializers.py
+++ backend/plinic/serializers.py
@@ -1,8 +1,14 @@
+from django.contrib.auth import get_user_model
 from rest_framework import serializers
-from .models import Post
+from .models import Post, Playlist
+
 
 class PostSerializer(serializers.ModelSerializer):
+    author = serializers.SerializerMethodField()
+
+    def get_author(self, obj):
+        return  {"nickname" : obj.profile.nickname, "profile_pic" : obj.profile.profile_pic if obj.profile.profile_pic else "프로필 사진이 없습니다."}
+
     class Meta:
         model = Post
         fields = "__all__"
-
