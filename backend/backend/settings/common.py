37bfe936b6ccfddc05496be9b848ab0509b589ea
TGoddessana <ingstor1092@gmail.com>
2022-07-31 21:16:12 +0900

    :card_file_box: 필수 파일 모델 작성 및 마이그레이션

--- backend/backend/settings/common.py
+++ backend/backend/settings/common.py
@@ -14,7 +14,9 @@ from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent.parent
-
+print(BASE_DIR)
+ROOT_DIR = os.path.dirname(BASE_DIR)
+print(ROOT_DIR)
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
