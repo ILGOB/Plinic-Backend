67bac3d7902e9d697edf0af822c48c1c70045266
TGoddessana <ingstor1092@gmail.com>
2022-08-24 20:11:50 +0900

    :recycle:(tgoddessana/backend/development) Admin에 Track 모델 추가

--- backend/plinic/admin.py
+++ backend/plinic/admin.py
@@ -1,24 +1,33 @@
 from django.contrib import admin
 
-from .models import Genre, Post, Notice, Playlist, Tag
+from .models import Genre, Post, Notice, Playlist, Tag, Track
 
 
 @admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
     pass
 
+
 @admin.register(Notice)
 class NoticeAdmin(admin.ModelAdmin):
     pass
+
 
 @admin.register(Playlist)
 class PostAdmin(admin.ModelAdmin):
     pass
 
+
 @admin.register(Genre)
 class GenreAdmin(admin.ModelAdmin):
     pass
 
+
 @admin.register(Tag)
 class TagAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Track)
+class TrackAdmin(admin.ModelAdmin):
     pass
