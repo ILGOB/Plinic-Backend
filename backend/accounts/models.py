a0586a7cc1fff3ad3e91228431cc05bd600e7699
TGoddessana <88619089+TGoddessana@users.noreply.github.com>
2022-08-24 21:01:41 +0900

    Revert "Feature/get custom post api/#16"

--- backend/accounts/models.py
+++ backend/accounts/models.py
@@ -1,24 +1,3 @@
-from django.conf import settings
 from django.db import models
 
-
-class TimeStampedModel(models.Model):
-    '''
-    모든 모델의 기본이 되는 TimeStampedModel
-    생성일자, 수정일자가 자동 추가되도록
-    '''
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
-
-    class Meta:
-        abstract = True
-
-
-class Profile(TimeStampedModel):
-    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-    nickname = models.CharField(max_length=150, unique=True, null=True)
-    profile_pic = models.ImageField(null=True, upload_to="profiles/%Y/%m/%d")
-
-
-    def __str__(self):
-        return f'{self.pk}:{self.user.username} Profile'
+# Create your models here.
--- backend/accounts/serializers.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import serializers
-from .models import Profile
-
-
-class ProfileSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Profile
-        fields = ['id', 'nickname', 'profile_pic']
