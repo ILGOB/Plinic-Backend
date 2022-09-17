63a35369120da09976ee8458f7bae0a1645104d4
hyeon <hyeon0697@gmail.com>
2022-09-17 15:56:50 +0900

    :art:(hyeon/feature/random-playlist-crud/#11) url통합, id값을통해 ud구현, 플레이리스트 get, post 통합

--- backend/plinic/urls.py
+++ backend/plinic/urls.py
@@ -7,7 +7,11 @@ router.register("posts", views.PostViewSet)
 
 urlpatterns = [
 
-    path('random-playlist', views.random_play_list_view),
-    path('random-playlist/', views.random_play_list_create),
+    #cr - get/post
+    path('playlist/', views.random_play_list_create),
+    #rud - get/put/delete
+    #detail 미구현, get요청처리 작성필요
+    path('playlist/<int:id>/', views.play_list_detail),
+
     path("", include(router.urls)),
 ]
--- backend/plinic/views.py
+++ backend/plinic/views.py
@@ -22,9 +22,10 @@ from .serializers import PostSerializer
 
 from datetime import timedelta
 
+PlaylistUser = list(Profile.objects.filter(id = 1))
+global profile_id
+profile_id = PlaylistUser[0]
 
-global total_urls
-total_urls = ""
 
 class PostViewSet(ModelViewSet):
     queryset = Post.objects.all()
@@ -40,6 +41,32 @@ class PostViewSet(ModelViewSet):
         return super().perform_create(serializer)
 
 class RandomPlaylistCreate(APIView):
+
+    def get(self, request):
+        if not 'genre' in request.GET or not 'num' in request.GET:
+            return Response({"error": "A 'genre' and 'num' is required."}, status=status.HTTP_400_BAD_REQUEST)
+
+        else:
+            # 장르가 우리가 지정한 장르가 아닐경우 오류메세지 출력
+            # 대소문자 구분 없이 정확히 일치하는 데이터 찾기
+            # if not plinic.objects.get(name__iexact="request.GET['genre']"):
+            #     return Response("This genre is not on the list.")
+            # else:
+            #     genre = request.GET['genre']
+            genre = request.GET['genre']
+            # 노래의 개수가 20개 이상일경우 오류메세지 출력
+            num = request.GET['num']
+            if int(num) >= 21:
+                return Response({"error": "More than 20 songs are not possible."}, status=status.HTTP_400_BAD_REQUEST)
+            else:
+                data = pl.random_playlist(genre, num)
+                data["profile_id"] = request.user.profile.id
+
+                ResponseData = Response(data)
+                print(request.user.profile.id)
+                
+
+                return ResponseData
     
     def post(self, request):
         data = json.loads(request.body)
@@ -52,6 +79,7 @@ class RandomPlaylistCreate(APIView):
         #######임시 플레이리스트 생성 시작
         #랜덤플레이리스트를 만들어낸 유저의 id정보
         PlaylistUser = list(Profile.objects.filter(id = data["profile_id"]))
+    
         # print(PlaylistUser[0])
         # print(type(PlaylistUser[0]))
         # print(type(request.user.profile))
@@ -92,43 +120,59 @@ class RandomPlaylistCreate(APIView):
 
             RandomPlayList_Track.save()
 
-        Playlist.objects.filter(title = titleName).filter(profile = PlaylistUser[0]).update(title = "포스트로 받아온 타이틀이름2!")
-
+        nowPlaylist = Playlist.objects.filter(title = titleName).filter(profile = PlaylistUser[0])
+        for qs in nowPlaylist:
+            print("id:{id}, title: {title}".format(**qs.__dict__))
+        nowPlaylist.update(title = "포스트로 받아온 타이틀이름3!")
 
         # RandomPlayList = Playlist.objects.filter(title = "tempList").filter(profile = PlaylistUser[0])
         
-
-
         return Response_json
+        
 random_play_list_create = RandomPlaylistCreate.as_view()
 
-class RandomPlayListView(APIView):
+class PlaylistDetail(APIView):
+    def put(self, request, **kwargs):
+        id = kwargs.get("id")
+        #profile 나중에 request.user.profile로 바꿔야함
+        Playlist.objects.filter(id = id).filter(profile = profile_id).update(title = "업데이트된 타이틀")
+        return Response()
+    def delete(self, request, **kwargs):
+        id = kwargs.get("id")
+        #profile 나중에 request.user.profile로 바꿔야함
+        Playlist.objects.filter(id = id).filter(profile = profile_id).delete()
+        return Response()
+play_list_detail = PlaylistDetail.as_view()
+
+
+#혹시몰라서 남겨둠, 플레이리스트로 통합됨.
+# class RandomPlayListView(APIView):
    
-    def get(self, request):
-        if not 'genre' in request.GET or not 'num' in request.GET:
-            return Response({"error": "A 'genre' and 'num' is required."}, status=status.HTTP_400_BAD_REQUEST)
+#     def get(self, request):
+#         if not 'genre' in request.GET or not 'num' in request.GET:
+#             return Response({"error": "A 'genre' and 'num' is required."}, status=status.HTTP_400_BAD_REQUEST)
 
-        else:
-            # 장르가 우리가 지정한 장르가 아닐경우 오류메세지 출력
-            # 대소문자 구분 없이 정확히 일치하는 데이터 찾기
-            # if not plinic.objects.get(name__iexact="request.GET['genre']"):
-            #     return Response("This genre is not on the list.")
-            # else:
-            #     genre = request.GET['genre']
-            genre = request.GET['genre']
-            # 노래의 개수가 20개 이상일경우 오류메세지 출력
-            num = request.GET['num']
-            if int(num) >= 21:
-                return Response({"error": "More than 20 songs are not possible."}, status=status.HTTP_400_BAD_REQUEST)
-            else:
-                data = pl.random_playlist(genre, num)
-                data["profile_id"] = request.user.profile.id
+#         else:
+#             # 장르가 우리가 지정한 장르가 아닐경우 오류메세지 출력
+#             # 대소문자 구분 없이 정확히 일치하는 데이터 찾기
+#             # if not plinic.objects.get(name__iexact="request.GET['genre']"):
+#             #     return Response("This genre is not on the list.")
+#             # else:
+#             #     genre = request.GET['genre']
+#             genre = request.GET['genre']
+#             # 노래의 개수가 20개 이상일경우 오류메세지 출력
+#             num = request.GET['num']
+#             if int(num) >= 21:
+#                 return Response({"error": "More than 20 songs are not possible."}, status=status.HTTP_400_BAD_REQUEST)
+#             else:
+#                 data = pl.random_playlist(genre, num)
+#                 data["profile_id"] = request.user.profile.id
 
-                ResponseData = Response(data)
-                print(request.user.profile.id)
+#                 ResponseData = Response(data)
+#                 print(request.user.profile.id)
                 
 
-                return ResponseData
-        # watch_videos?video_ids=
+#                 return ResponseData
+#         # watch_videos?video_ids=
 
-random_play_list_view = RandomPlayListView.as_view()
+# random_play_list_view = RandomPlayListView.as_view()
