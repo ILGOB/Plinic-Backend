d4d8ab908bd1aeecd5f4bdaeda766b1ea58da2ca
TGoddessana <ingstor1092@gmail.com>
2022-09-27 09:39:19 +0900

    :sparkles: (tgoddessana/develop)  합치는 중...

--- backend/accounts/urls.py
+++ backend/accounts/urls.py
@@ -6,8 +6,9 @@ router = SimpleRouter()
 router.register("profiles", views.ProfileViewSet)
 
 urlpatterns = [
-    # path("", include(router.urls)),
-    path('kakao-authentication/login/', views.kakao_login),
-    path('kakao-authentication/callback/', views.kakao_callback),
-    path('kakao-authentication/login/finish/', views.KakaoLogin.as_view()),
+    path("", include(router.urls)),
+    path('kakao-authentication/login/', views.kakao_login_view, name='kakao_login'),
+    path('kakao-authentication/callback/', views.kakao_callback_view, name='kakao_callback'),
+    path('kakao-authentication/login/finish/', views.KakaoLoginView.as_view(), name='kakao_login_finish'),
 ]
+
