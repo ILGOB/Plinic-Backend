ccbdc158a23de5a82fb42974c3bf369c9c4de65f
TGoddessana <ingstor1092@gmail.com>
2022-10-12 20:54:40 +0900

    배포 설정 테스트

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -15,11 +15,11 @@ urlpatterns = [
     path('api/v1/accounts/', include('allauth.urls')),
 ]
 
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-
-    import debug_toolbar
-
-    urlpatterns += [
-        path('__debug__/', include(debug_toolbar.urls))
-    ]
+# if settings.DEBUG:
+#     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+#
+#     import debug_toolbar
+#
+#     urlpatterns += [
+#         path('__debug__/', include(debug_toolbar.urls))
+#     ]
