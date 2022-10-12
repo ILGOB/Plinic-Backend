ee694cc795a4f2c4919078a86d70f97cc4a3eea4
TGoddessana <ingstor1092@gmail.com>
2022-10-12 20:22:36 +0900

    배포 설정 테스트

--- backend/backend/wsgi.py
+++ backend/backend/wsgi.py
@@ -8,10 +8,13 @@ https://docs.djangoproject.com/en/3.2/howto/deployment/wsgi/
 """
 
 import os
-
+import sys
 from django.core.wsgi import get_wsgi_application
 
-os.environ['PYTHONPATH'] = os.path.abspath(os.path.dirname(__file__))
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.settings.dev')
+
+sys.path.append('../../../Plinic-Backend')
+sys.path.append('venv/lib/python3.10/site-packages')
+sys.path.append('../../backend')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.backend.settings.dev')
 
 application = get_wsgi_application()
