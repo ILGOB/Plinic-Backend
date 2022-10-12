60fbe843d09a12a8c3f718ff8eaf7dbba225bf8d
TGoddessana <ingstor1092@gmail.com>
2022-10-12 20:56:07 +0900

    배포 설정 테스트

--- backend/backend/settings/dev.py
+++ backend/backend/settings/dev.py
@@ -4,8 +4,8 @@ INSTALLED_APPS += [
     # 'debug_toolbar',
 ]
 
-MIDDLEWARE = [
-    'debug_toolbar.middleware.DebugToolbarMiddleware',
-] + MIDDLEWARE
+# MIDDLEWARE = [
+#     'debug_toolbar.middleware.DebugToolbarMiddleware',
+# ] + MIDDLEWARE
 
 INTERNAL_IPS = ["127.0.0.1"]
