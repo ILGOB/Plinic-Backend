9bb233414d26e29dab00463c69f3fa13da64f14c
TGoddessana <ingstor1092@gmail.com>
2022-10-25 14:35:46 +0900

    fear:

--- backend/backend/wsgi.py
+++ backend/backend/wsgi.py
@@ -13,10 +13,6 @@ from pathlib import Path
 
 from django.core.wsgi import get_wsgi_application
 
-
-sys.path.append(str(Path(__file__).resolve().parent.parent.parent))
-sys.path.append(str(Path(__file__).resolve().parent.parent))
-sys.path.append(str(Path(__file__).resolve().parent))
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "backend.settings.dev")
 
 application = get_wsgi_application()
